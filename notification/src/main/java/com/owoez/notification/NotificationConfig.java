ae4ab902174b4e52ddda2370cdc383d391c242fa
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2022-03-17 15:58:27 +0100

    Added notification queue configuration

---
+++ notification/src/main/java/com/owoez/notification/NotificationConfig.java
@@ -0,0 +1,45 @@
+package com.owoez.notification;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author: tobilobaowolabi
+ * Date: 17/03/2022
+ * Project: distributed-services
+ * IDE: IntelliJ IDEA
+ **/
+
+@Configuration
+public class NotificationConfig {
+  @Value("{rabbitmq.exchanges.internal}")
+  private String internalExchange;
+  @Value("{rabbitmq.queue.notification}")
+  private String notificationQueue;
+  @Value("{rabbitmq.routing-keys.internal-notification}")
+  private String internalNotificationRoutingKey;
+
+  public String getInternalExchange() {
+    return internalExchange;
+  }
+
+  public void setInternalExchange(String internalExchange) {
+    this.internalExchange = internalExchange;
+  }
+
+  public String getNotificationQueue() {
+    return notificationQueue;
+  }
+
+  public void setNotificationQueue(String notificationQueue) {
+    this.notificationQueue = notificationQueue;
+  }
+
+  public String getInternalNotificationRoutingKey() {
+    return internalNotificationRoutingKey;
+  }
+
+  public void setInternalNotificationRoutingKey(String internalNotificationRoutingKey) {
+    this.internalNotificationRoutingKey = internalNotificationRoutingKey;
+  }
+}
