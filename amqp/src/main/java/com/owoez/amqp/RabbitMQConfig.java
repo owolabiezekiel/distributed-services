b44a9137931330438263123e7e80bc109bea78d4
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2022-03-17 02:16:43 +0100

    Added AMQP template and JacksonCOnverter for basic message converter

---
+++ amqp/src/main/java/com/owoez/amqp/RabbitMQConfig.java
@@ -0,0 +1,36 @@
+package com.owoez.amqp;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author: tobilobaowolabi
+ * Date: 17/03/2022
+ * Project: distributed-services
+ * IDE: IntelliJ IDEA
+ **/
+
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfig {
+  private final ConnectionFactory connectionFactory;
+
+  @Bean
+  public AmqpTemplate amqpTemplate() {
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(jacksonConverter());
+    return rabbitTemplate;
+  }
+
+  @Bean
+  public MessageConverter jacksonConverter() {
+    MessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+    return jackson2JsonMessageConverter;
+  }
+}
