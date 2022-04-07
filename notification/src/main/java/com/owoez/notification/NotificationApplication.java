83eb823c27b5e20c239e5749502529ca4ba2d664
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2022-04-07 18:45:22 +0100

    Publishing messages to Rabbit MQ. testing it with Notification service

--- notification/src/main/java/com/owoez/notification/NotificationApplication.java
+++ notification/src/main/java/com/owoez/notification/NotificationApplication.java
@@ -1,8 +1,11 @@
 package com.owoez.notification;
 
+import com.owoez.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Author: tobilobaowolabi
@@ -11,10 +14,24 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * IDE: IntelliJ IDEA
  **/
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = {
+		"com.owoez.notification",
+		"com.owoez.amqp"
+	}
+)
 @EnableEurekaClient
 public class NotificationApplication {
   public static void main(String[] args) {
 	SpringApplication.run(NotificationApplication.class, args);
   }
+
+//  @Bean
+//  CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig){
+//	return args -> {
+//	  producer.publish(new Person("Tobiloba", 20), notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+//	};
+//  }
+//
+//  record Person(String name, int age){}
 }
